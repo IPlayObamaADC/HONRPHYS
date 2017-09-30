49aae46db3134274cfbcb6c3dc713234cccf7365
Linden <lyuan12@umd.edu>
2017-09-30 10:05:01 -0400

    add program examples demonstrating loops

---
+++ forLoops1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n=0, m=0;
+
+  while(n<10) {
+    cout << "n is " << n << ": ";
+    m=0;
+
+    while(m<=n) {
+      cout << m;
+      m++;
+    }
+
+    cout << endl;
+    n++;
+  }
+
+  return 0;
+}
