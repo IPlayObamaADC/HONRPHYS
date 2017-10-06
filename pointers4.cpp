c74d50675dd3bc2e290ab570573e4a4d61d1881a
Linden <lyuan12@umd.edu>
2017-10-06 12:22:15 -0400

    Add pointers exercise

---
+++ pointers4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double* p1 = new double(3.14);
+  double* p2 = p1;
+  cout << "The data stored in address " << p1 << "is " << *p1 << endl;
+  cout << "The data stored in address " << p2 << "is " << *p2 << endl;
+
+  *p1 *= 2;
+  cout << "The data stored in address " << p1 << "is " << *p1 << endl;
+  cout << "The data stored in address " << p2 << "is " << *p2 << endl;
+}
